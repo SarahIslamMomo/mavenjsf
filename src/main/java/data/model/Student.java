d051fb1df188d0b41e237938f256cee89225e547
BUP <saarahmomo76@gmail.com>
2018-04-23 01:52:33 -0700

    mavenjsf

---
+++ src/main/java/data/model/Student.java
@@ -0,0 +1,34 @@
+package data.model;
+
+public class Student {
+    private String studentFirstName;
+    private String studentLastName;
+    private int studentId;
+
+    public Student() {
+    }
+
+    public String getStudentFirstName() {
+        return studentFirstName;
+    }
+
+    public void setStudentFirstName(String studentFirstName) {
+        this.studentFirstName = studentFirstName;
+    }
+
+    public String getStudentLastName() {
+        return studentLastName;
+    }
+
+    public void setStudentLastName(String studentLastName) {
+        this.studentLastName = studentLastName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+}
